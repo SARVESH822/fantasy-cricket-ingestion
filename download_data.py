802399447b10038095fa2b69bd21315737a91742
SARVESH <sarvesh822006@gmail.com>
2026-05-12 18:25:47 +0530

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -5,25 +5,48 @@ import shutil
 
 leagues = {
 
-    # Franchise leagues
+    # IPL
     "ipl": "https://cricsheet.org/downloads/ipl_json.zip",
+
+    # Big Bash League
     "bbl": "https://cricsheet.org/downloads/bbl_json.zip",
+
+    # Pakistan Super League
     "psl": "https://cricsheet.org/downloads/psl_json.zip",
+
+    # Caribbean Premier League
     "cpl": "https://cricsheet.org/downloads/cpl_json.zip",
-    "lpl": "https://cricsheet.org/downloads/lpl_json.zip",
+
+    # Bangladesh Premier League
     "bpl": "https://cricsheet.org/downloads/bpl_json.zip",
 
-    # International
+    # Lanka Premier League
+    "lpl": "https://cricsheet.org/downloads/lpl_json.zip",
+
+    # Vitality Blast (England)
+    "blast": "https://cricsheet.org/downloads/vitalityblast_json.zip",
+
+    # Syed Mushtaq Ali Trophy
+    "smat": "https://cricsheet.org/downloads/sma_male_json.zip",
+
+    # International T20s
     "t20i": "https://cricsheet.org/downloads/t20s_json.zip",
 
-    # Domestic
-    "smat": "https://cricsheet.org/downloads/smat_json.zip",
+    # SA20
+    "sa20": "https://cricsheet.org/downloads/sa20_male_json.zip",
 
-    # England T20 Blast
-    "blast": "https://cricsheet.org/downloads/vitalityblast_json.zip"
+    # ILT20
+    "ilt20": "https://cricsheet.org/downloads/ilt20_male_json.zip",
+
+    # The Hundred
+    "hundred": "https://cricsheet.org/downloads/hundred_male_json.zip",
+
+    # Super Smash (New Zealand)
+    "supersmash": "https://cricsheet.org/downloads/super_smash_male_json.zip",
+
+    # T10 leagues
+    "abu_dhabi_t10": "https://cricsheet.org/downloads/abt10_male_json.zip"
 }
-
-
 # Create folders
 os.makedirs("raw_data", exist_ok=True)
 
